4c6ba78573b2b78345bef5c88bddd66934eb116e
Biniyam Ambachew <biniyamambachew@gmail.com>
2023-12-19 13:42:16 +0300

    update

---
+++ home.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.4.0/remixicon.css" crossorigin="">
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <!-- side bar -->
+    <div class="side_bar_container">
+        <ul class="nav_list">
+            <li>
+                <a href="#">
+                <i class="ri-account-circle-line"></i>
+                USER NAME
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="ri-search-line"></i>
+                <input type="text" placeholder="SEARCH">
+                </a>
+            </li>
+            <li>
+                <a href="home.php">
+                <i class="ri-home-4-fill"></i>
+                HOME
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="ri-film-line"></i>
+                MOVIES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="ri-slideshow-3-line"></i>
+                SHOWS
+                </a>
+            </li>
+            <li>
+                <a href="">
+                <i class="ri-settings-3-line"></i>
+                SETTINGS
+                </a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
